b89e5ee9e1c08740cf391d0dabb104f633306ede
Randal Vance Cunanan <randal@randalvance.net>
2019-11-10 22:18:45 +0800

    Use Formik for Add Expense Page

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,17 +1,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form } from 'react-bootstrap';
 import ReactDayPickerInput from 'react-day-picker/DayPickerInput';
 
 import '../styles/react-date-picker.scss';
 
-export const DatePicker: React.FC = () => (
-  <ReactDayPickerInput
-    classNames={{
-      container: 'DayPickerInput w-100',
-      overlayWrapper: 'DayPickerInput-OverlayWrapper',
-      overlay: 'DayPickerInput-Overlay',
-    }}
-    component={(props: any) => <Form.Control type="text" {...props} placeholder="Date" />}
-  />
-);
+type Props = {
+  value?: Date;
+  onChange?: (date: Date) => void;
+};
+
+const DatePicker: React.FC<Props> = ({ value, onChange }, ref) => {
+  const handleDayChanged = useCallback((date) => {
+    if (onChange !== undefined) {
+      onChange(date);
+    }
+  }, [onChange]);
+  return (
+    <ReactDayPickerInput
+      ref={ref}
+      value={value as any}
+      onDayChange={handleDayChanged}
+      classNames={{
+        container: 'DayPickerInput w-100',
+        overlayWrapper: 'DayPickerInput-OverlayWrapper',
+        overlay: 'DayPickerInput-Overlay',
+      }}
+      component={(componentProps: any) => (
+        <Form.Control type="text" {...componentProps} placeholder="Date" />
+      )}
+    />
+  );
+};
+
+export default React.forwardRef(DatePicker);
--- src/components/features/expenses/pages/ExpensesEditPage.tsx
+++ src/components/features/expenses/pages/ExpensesEditPage.tsx
@@ -2,26 +2,58 @@ import React from 'react';
 import {
   Button, Col, Form, Row,
 } from 'react-bootstrap';
+import { useFormik } from 'formik';
 
-import { DatePicker } from 'components';
+import DatePicker from 'components/DatePicker';
 
-export const ExpensesEditPage: React.FC = () => (
-  <Form>
-    <Row>
-      <Col sm={12} md={6}>
-        <Row noGutters>
-          <Col sm={12} className="p-1">
-            <Form.Control type="text" placeholder="Name" />
-          </Col>
-          <Col sm={12} className="p-1">
-            <DatePicker />
-          </Col>
-          <Col sm={12} className="p-1">
-            <Form.Control type="number" placeholder="Amount" />
-          </Col>
-          <Col className="p-1 pt-3"><Button>Add</Button></Col>
-        </Row>
-      </Col>
-    </Row>
-  </Form>
-);
+export const ExpensesEditPage: React.FC = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: 'My Expense',
+      date: new Date(),
+      amount: 300,
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Row>
+        <Col sm={12} md={6}>
+          <Row noGutters>
+            <Col sm={12} className="p-1">
+              <Form.Control
+                name="name"
+                type="text"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Name"
+              />
+            </Col>
+            <Col sm={12} className="p-1">
+              <DatePicker
+                value={formik.values.date}
+                onChange={(value) => formik.setFieldValue('date', value)}
+              />
+            </Col>
+            <Col sm={12} className="p-1">
+              <Form.Control
+                name="amount"
+                type="number"
+                value={formik.values.amount.toString()}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Amount"
+              />
+            </Col>
+            <Col className="p-1 pt-3">
+              <Button type="submit">Add</Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
