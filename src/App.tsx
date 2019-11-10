ea9e43fbf3bc1cb09cb705e21913a9432e4c673b
Randal Vance Cunanan <randal@randalvance.net>
2019-11-10 14:30:36 +0800

    Add React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
+import {
+  Button, Col, Container, Row,
+} from 'react-bootstrap';
 
 const App: React.FC = () => (
   <div className="App">
-    <header className="App-header">
-      ExpenSee
-    </header>
+    <Container fluid>
+      <Row>
+        <Col>Col 1</Col>
+        <Col>Col 2</Col>
+      </Row>
+      <Row>
+        <Col><Button>Test</Button></Col>
+      </Row>
+    </Container>
   </div>
 );
 
